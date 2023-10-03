8f065631b2de9f67ac7fa60264c7ebcaddded9d0
enzoimola <ennzoimola@hotmail.com>
2023-10-03 03:46:21 -0300

    improve code

--- layouts/MainLayout.tsx
+++ layouts/MainLayout.tsx
@@ -1,61 +1,57 @@
 import { AppShell } from '@mantine/core';
 
 import React, { PropsWithChildren, useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { ApolloQueryResult } from '@apollo/client';
+import { useDispatch } from 'react-redux';
+import { ApolloQueryResult, NoInfer, QueryResult, useQuery } from '@apollo/client';
+import { ApolloError } from '@apollo/client/errors';
 import { Header } from '@/components/molecules/Header/Header';
-import { createApolloClient } from '@/apollo-client';
 import { FETCH_FAVORITES_MEDIA, FETCH_MEDIA } from '@/graphql/queries';
-import { selectFavourites, setFavoritesMedia, setMedia } from '@/store/dataSlice';
+import { setFavoritesMedia, setMedia } from '@/store/dataSlice';
 import SkeletonHeader from '@/components/atoms/SkeletonHeader';
 import { PageNoData } from '@/components/atoms/PageNoData/PageNoData';
 import { IMedia } from '@/models/interfaces/media/media.interface';
 import { IFavsUser } from '@/models/interfaces/media/favMedia.interface';
-
-type GetMediaType = {
-    media: Array<IMedia>;
-};
 
 type GetFavoritesType = {
     getFavorites: Array<IFavsUser>;
 };
 
 export const MainLayout: React.FC<PropsWithChildren> = ({ children }) => {
-    const client = createApolloClient;
     const dispatch = useDispatch();
-    const favs: Array<IMedia> = useSelector(selectFavourites);
+    const [loadingFavs, setLoadingFavs] = useState<boolean>(true);
 
-    const [loadingFavs, setLoadingFavs] = useState<boolean>(false);
-
-    // eslint-disable-next-line consistent-return
-    const fetchMedia = async () => {
-        try {
-            const { data: media }: ApolloQueryResult<GetMediaType> = await client.query({
-                query: FETCH_MEDIA,
+    const { data: media, loading: loadingMedia, error: errorMedia }:
+        QueryResult<{ media: Array<IMedia>, loadingMedia: boolean, error: ApolloError }> =
+            useQuery(FETCH_MEDIA, {
+                onCompleted: (resp) => {
+                    dispatch(setMedia(resp.media));
+                },
             });
-            const { data: favorites }: ApolloQueryResult<GetFavoritesType> = await client.query({
-                query: FETCH_FAVORITES_MEDIA(localStorage.getItem('uid')),
-            });
-            setLoadingFavs(favorites.getFavorites.length >= 0);
 
-            const idSet = new Set(favorites.getFavorites.map(item => item.id));
-            const commonItems = media.media.filter(item => idSet.has(item.id));
-            dispatch(setMedia(media.media));
-            dispatch(setFavoritesMedia(commonItems));
-        } catch (e) {
-            return (
-                <PageNoData
-                  text="Something bad just happened.."
-                  title={"Our servers could not handle your request. Don't worry, our development team was already notified. Try refreshing the page"}
-                  returnBack
-                />
-                );
-        }
-    };
+    const { data: favorites, loading: loadingFav, error: errorFavorites }:
+        QueryResult<{ favorites: GetFavoritesType, loadingFav: boolean, error: ApolloError }> =
+            useQuery(FETCH_FAVORITES_MEDIA(localStorage.getItem('uid')));
 
     useEffect(() => {
-        fetchMedia().then();
-    }, []);
+        if (!favorites || !media) return;
+
+        const { getFavorites } = favorites;
+
+        setLoadingFavs(getFavorites.length >= 0);
+
+        const idSet = new Set(getFavorites.map(item => item.id));
+        const commonItems = media.media.filter(item => idSet.has(item.id));
+        dispatch(setMedia(media.media));
+        dispatch(setFavoritesMedia(commonItems));
+    }, [loadingFav || loadingMedia]);
+
+    if (errorMedia || errorFavorites) {
+        return (<PageNoData
+          text="Something bad just happened.."
+          title={"Our servers could not handle your request. Don't worry, our development team was already notified. Try refreshing the page"}
+          returnBack
+        />);
+    }
 
     return (
         <AppShell
