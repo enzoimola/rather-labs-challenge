6ce826adf13b3ea1beaa4d63090fe36f352db196
enzoimola <ennzoimola@hotmail.com>
2023-10-03 15:50:31 -0300

    fix route on not allowed page

--- components/atoms/NotAllowedAccess/index.tsx
+++ components/atoms/NotAllowedAccess/index.tsx
@@ -8,7 +8,7 @@ const NotFoundTitle: React.FC = () => {
     const [loading, setLoading] = useState<boolean>(false);
     const onGoLoginHandler = () => {
         setLoading(true);
-        router.replace('/home').then();
+        router.replace('/').then();
     };
     return (
         <Container className={classes.root}>
